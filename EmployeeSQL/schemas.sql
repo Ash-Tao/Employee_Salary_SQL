a9edc8ceb994fdfd53cde3087c022b10da2888ec
Ash <ash.taowq@gmail.com>
2022-04-03 18:08:25 +1000

    Start

---
+++ EmployeeSQL/schemas.sql
@@ -0,0 +1,75 @@
+alter table employees drop constraint if exists fk_emp_title;
+alter table dept_manager drop constraint if exists fk_emp_no_manager;
+alter table dept_emp drop constraint if exists fk_emp_no;
+alter table dept_manager drop constraint if exists fk_dept_no_manager;
+alter table dept_emp drop constraint if exists fk_dept_no;
+
+
+drop table if exists departments;
+drop table if exists dept_emp;
+drop table if exists dept_manager;
+drop table if exists employees;
+drop table if exists salaries;
+drop table if exists titles;
+
+create table departments (
+	dept_no varchar(4) not null,
+	dept_name varchar(50) not null,
+	primary key (dept_no)
+);
+
+create table titles (
+	title_id varchar(5) not null,
+	title varchar(50) not null,
+	primary key (title_id)
+);
+
+create table salaries (
+	emp_no int not null,
+	salary int not null,
+	primary key (emp_no)
+);
+
+create table employees (
+	emp_no int not null,
+	emp_title_id varchar(5) not null,
+	birth_date date not null,
+	first_name varchar(50) not null,
+	last_name varchar(50) not null,
+	sex varchar(1) not null,
+	hire_date date not null,
+	primary key (emp_no)
+);
+
+create table dept_emp (
+	emp_no int not null,
+	dept_no varchar(4) not null,
+	primary key (emp_no, dept_no)
+);
+
+create table dept_manager (
+	dept_no varchar(4) not null,
+	emp_no int not null,
+	primary key (emp_no, dept_no)
+);
+
+
+alter table employees add constraint fk_emp_title
+foreign key (emp_title_id)
+references titles(title_id);
+
+alter table dept_manager add constraint fk_emp_no_manager
+foreign key (emp_no)
+references employees(emp_no);
+
+alter table dept_emp add constraint fk_emp_no
+foreign key (emp_no)
+references employees(emp_no);
+
+alter table dept_manager add constraint fk_dept_no_manager
+foreign key (dept_no)
+references departments(dept_no);
+
+alter table dept_emp add constraint fk_dept_no
+foreign key (dept_no)
+references departments(dept_no);
